3a4af275f141b8430fe2fd040d023c77b89e6ce0
danielorangeviana <danieloviana@gmail.com>
2023-10-30 18:58:34 -0300

    findById implementation

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,21 +1,16 @@
 package Application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department object = new Department(1, "book");
-		
-		Seller seller = new Seller(21, "Daniel", "daniel@gmail.com", new Date(), 3000.0, object);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 		
--- src/model/dao/implement/SellerDaoJDBC.java
+++ src/model/dao/implement/SellerDaoJDBC.java
@@ -1,12 +1,25 @@
 package model.dao.implement;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao{
-
+	
+	private Connection connection;
+	
+	public SellerDaoJDBC(Connection connection) {
+		this.connection = connection;
+	}
+	
 	@Override
 	public void insert(Seller object) {
 		// TODO Auto-generated method stub
@@ -27,8 +40,46 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement prStatement = null;
+		ResultSet resultSet = null;
+		
+		try {
+			prStatement = connection.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+"WHERE seller.Id = ?");
+			
+			prStatement.setInt(1, id);
+			resultSet = prStatement.executeQuery(); //Executa a query 'SELECT' acima.
+			
+			if(resultSet.next()) {
+				// Instanciando o objeto Department
+				Department department = new Department();
+				department.setId(resultSet.getInt("DepartmentId"));
+				department.setName(resultSet.getString("DepName"));
+				
+				// Instanciando o objeto Seller
+				Seller seller = new Seller();
+				seller.setId(resultSet.getInt("Id"));
+				seller.setName(resultSet.getString("Name"));
+				seller.setEmail(resultSet.getString("Email"));
+				seller.setBirthDate(resultSet.getDate("BirthDate"));
+				seller.setBaseSalary(resultSet.getDouble("BaseSalary"));
+				
+				// Associando os objetos acima
+				seller.setDepartement(department);
+				return seller;
+			}
+			return null;
+		}
+		catch (SQLException error) {
+			throw new DbException(error.getMessage());
+		}
+		finally {
+			DB.closeStatement(prStatement);
+			DB.closeResultSet(resultSet);
+		}
 	}
 
 	@Override
